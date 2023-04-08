b45e42df97a268dd4fc20077753d08a7a5bbcfc3
OrawusiTimilehin <timilehin.orawusi@gmail.com>
2023-04-08 20:31:20 +0100

    Completed the contact page

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'nviwbv8(*403gjBVEI^@Go2u@f2GU^@%B39(+_!£)BTV'
+Bootstrap(app)
 
 
 class ContactForm(FlaskForm):
@@ -19,9 +22,23 @@ class ContactForm(FlaskForm):
 def home():
     return render_template("index.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    contact_form = ContactForm()
+    error = None
+    if request.method == 'POST':
+        data = request.form
+        if '@' in data['email']:
+            email = data['email']
+            name = data['name']
+            message = data['message']  
+            additional_info = data['additional_info']
+            return render_template("successful.html")
+        else:
+            error = "Please Enter in a valid Email"
+            return render_template("contact.html", form=contact_form, error=error)
+
+    return render_template("contact.html" , form=contact_form, error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
